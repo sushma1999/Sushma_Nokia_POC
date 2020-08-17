1131a317e655c9c51f914429b17a5ed9f119cda7
sushma1999 <56186986+sushma1999@users.noreply.github.com>
2020-08-17 19:16:35 +0530

    Initial commit

---
+++ Assets/Scenes/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // To control camera
+
+        public GameObject Player;
+        private Vector3 offset;// offset to get the distance from the player object 
+    void Start()
+    {
+        offset = transform.position;
+    }
+    private void LateUpdate()
+    {
+        //lets add the offset to follow the player movement
+        transform.position = Player.transform.position + offset;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float speed;
+	private int count = 0;
+
+	public Text scoreText;
+	public Text WinText;
+	private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		rigidbody = GetComponent<Rigidbody>();
+
+	}
+
+	void FixedUpdate()
+	{
+
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void LateUpdate()
+	{
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		if (other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count += 1;
+			SetCountText();
+
+
+		}
+	}
+
+	void SetCountText()
+	{
+		
+		scoreText.text = "Score : " + count;
+
+		//Check whether you have all the pickups
+
+		if (count >= 23)
+		{
+			WinText.gameObject.SetActive(true);
+			WinText.text = " You Win !!! ";
+		}
+	}
+}
